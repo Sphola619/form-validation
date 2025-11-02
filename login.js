f90ca73c1fb78df86274ff1702464c1fbf50a0d3
Sipho <sisusasipho@gmail.com>
2025-11-02 23:39:28 +0200

    backend routes updated

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ form.addEventListener("submit", async (e) => {
   const password = document.getElementById("password").value.trim();
 
   try {
-    const response = await fetch("http://localhost:5000/login", {
+    const response = await fetch("https://form-validation-backend.onrender.com/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
--- register.js
+++ register.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", async (e) => {
   }
 
   try {
-    const response = await fetch("http://localhost:5000/register", {
+    const response = await fetch("https://form-validation-backend.onrender.com/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
